8f259d3ee6bf06442c4507feb8d0b8c0825c7bb1
Lakshmi Banka <127284521+Lakshmi-banka@users.noreply.github.com>
2025-02-09 23:54:29 +0530

    Update get_pet_labels.py

--- Ml model/get_pet_labels.py
+++ Ml model/get_pet_labels.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
+# PROGRAMMER: Lakshmi Banka
+# DATE CREATED: 10-11-2024                               
+# REVISED DATE: 09-02-2025
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
@@ -20,17 +20,14 @@
 import os
 from os import listdir
 
-# TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
-#       with this function
-# 
+#       Define get_pet_labels function below.
 def get_pet_labels(image_dir):
     """
     Creates a dictionary of pet labels (results_dic) based upon the filenames 
     of the image files. These pet image labels are used to check the accuracy 
     of the labels that are returned by the classifier function, since the 
     filenames of the images contain the true identity of the pet in the image.
-    Be sure to format the pet labels so that they are in all lower case letters
+    Format the pet labels so that they are in all lower case letters
     and with leading and trailing whitespace characters stripped from them.
     (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
     Parameters:
@@ -66,6 +63,4 @@ def get_pet_labels(image_dir):
             results_dic[filename] = [pet_label]
         else:
             print("** Warning: Duplicate files exist in directory:", filename)
-    # Replace None with the results_dic dictionary that you created with this
-    # function
     return results_dic
