30a4a5012b4443612a9357503f663863d4ec58d6
Lakshmi Banka <127284521+Lakshmi-banka@users.noreply.github.com>
2025-02-10 00:13:03 +0530

    Update get_pet_labels.py

--- Ml model/get_pet_labels.py
+++ Ml model/get_pet_labels.py
@@ -3,8 +3,8 @@
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
 # PROGRAMMER: Lakshmi Banka
-# DATE CREATED: 10-09-2024                               
-# REVISED DATE: 10-12-2024
+# DATE CREATED: 11/09/2024                               
+# REVISED DATE: 11/12/2024
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
