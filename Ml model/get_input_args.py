6751e36570f268a3e93e371317f50be67ce6786f
Lakshmi Banka <127284521+Lakshmi-banka@users.noreply.github.com>
2025-02-10 00:12:39 +0530

    Update get_input_args.py

--- Ml model/get_input_args.py
+++ Ml model/get_input_args.py
@@ -3,8 +3,8 @@
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
 # PROGRAMMER: Lakshmi Banka
-# DATE CREATED: 10-09-2024                                
-# REVISED DATE: 10-12-2024
+# DATE CREATED: 11/09/2024                                
+# REVISED DATE: 11/12/2024
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
 #          provide some or all of the 3 inputs, then the default values are
